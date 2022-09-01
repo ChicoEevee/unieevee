d91867353ca9bdcb455669570afe3e8e29806b59
ElChicoEevee <kirbyyeray@hotmail.com>
2022-09-01 22:04:29 +0200

    Update script.js

--- Skins/script.js
+++ Skins/script.js
@@ -6,7 +6,7 @@ for (let badge of badges) {
 	a.classList.add('animal')
 	let t = `<p class="titlecolor"><strong> ${badge.Name}</strong></p>
 	<p><strong>Obtainable through:</strong> ${badge.StageObtainWay}</p>
-	<p><strong>Quality</strong><img class="pet-photo2" src="images/${badge.Quality}.png"></p>`
+	<p><strong>Quality</strong><img class="pet-photo2" src="../images/${badge.Quality}.png"></p>`
 
 	for (let x of badge.IconPath) {
 		t += `<img class="pet-photo" src="../images/${x}.png">`
