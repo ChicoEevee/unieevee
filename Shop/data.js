2b50018df44d3559648c2e039c2fb385653334ed
ElChicoEevee <kirbyyeray@hotmail.com>
2022-09-01 21:56:11 +0200

    hm

---
+++ Shop/data.js
@@ -0,0 +1,255 @@
+const badges=[{
+        "Name": "Trainer battle ticket",
+        "Data": {
+        "Desc": "A ticket that enables you to participate in Legacy Trainer Showdown",
+		"Release Date": "10-09-2022 00:00:00 UTC",
+        "AvailableUntil": "06-10-2022 23:59:59 UTC",
+        "Coins": "200",
+		},
+        "type": ["t_common_icon_090201_TCTicket_54e0c9c52bfc50d9"],
+	},
+    {
+        "Name": "Tuxedo Style: Garchomp",
+        "Data": {
+		"Release Date": "05-09-2022 00:00:00 UTC",
+        "Gems": "1200",
+		},
+        "type": ["t_Tachie_Garchomp_10_b24e40c57daff61c"],
+	},
+    {
+        "Name": "Unite License & Tuxedo Style Garchomp",
+        "Data": {
+		"Release Date": "05-09-2022 00:00:00 UTC",
+        "AvailableUntil": "11-09-2022 23:59:59 UTC",
+        "Gems": "1775",
+        "Discounted Gems Price": "1509",
+        "Discount Start": "05-09-2022 00:00:00 UTC",
+        "Discount End": "11-09-2022 23:59:59 UTC",
+		},
+        "type": ["t_Tachie_Garchomp_00", "t_Tachie_Garchomp_10_b24e40c57daff61c"],
+	},
+    {
+        "Name": "Tuxedo Style: Glaceon",
+        "Data": {
+		"Release Date": "19-09-2022 00:00:00 UTC",
+        "Gems": "1050",
+		},
+        "type": ["t_Tachie_Glaceon_03_b804278bec7a5053"],
+	},
+    {
+        "Name": "Purple Unite Style: Espeon",
+        "Data": {
+        "Release Date": "02-09-2022 00:00:00 UTC",
+        "Gems": "350",
+		},
+        "type": ["t_Tachie_Espeon_03"],
+	},
+    {
+        "Name": "Orange Unite Style: Espeon",
+        "Data": {
+		"Release Date": "02-09-2022 00:00:00 UTC",
+        "Gems": "350",
+		},
+        "type": ["t_Tachie_Espeon_04"],
+	},
+    {
+        "Name": "Unite License: Dodrio",
+        "Data": {
+        "Desc": "Grants permission to take Dodrio into battle.",
+		"Release Date": "15-09-2022 00:00:00 UTC",
+        "AvailableUntil": "21-09-2022 23:59:59 UTC",
+        "Gems": "575",
+		},
+        "type": ["t_Tachie_Dodrio_00_187d0db485dabbfd"],
+	},
+    {
+        "Name": "Unite License: Dodrio",
+        "Data": {
+        "Desc": "Grants permission to take Dodrio into battle.",
+		"Release Date": "22-09-2022 00:00:00 UTC",
+        "Coins": "14000",
+        "Gems": "575",
+		},
+        "type": ["t_Tachie_Dodrio_00_187d0db485dabbfd"],
+	},
+    {
+        "Name": "Purple Unite Style: Dodrio",
+        "Data": {
+		"Release Date": "15-09-2022 00:00:00 UTC",
+        "Gems": "350",
+		},
+        "type": ["t_Tachie_Dodrio_01_0ffa64688ffc37ae"],
+	},
+    {
+        "Name": "Unite License & Purple Unite Style: Dodrio",
+        "Data": {
+		"Release Date": "15-09-2022 00:00:00 UTC",
+        "AvailableUntil": "25-09-2022 23:59:59 UTC",
+        "Gems": "925",
+        "Discounted Gems Price": "786",
+        "Discount Start": "15-09-2022 00:00:00 UTC",
+        "Discount End": "25-09-2022 23:59:59 UTC",
+		},
+        "type": ["t_Tachie_Dodrio_00_187d0db485dabbfd", "t_Tachie_Dodrio_01_0ffa64688ffc37ae"],
+	},
+    {
+        "Name": "Orange Unite Style: Dodrio",
+        "Data": {
+		"Release Date": "15-09-2022 00:00:00 UTC",
+        "Gems": "350",
+		},
+        "type": ["t_Tachie_Dodrio_02_c22ebbc44c65e666"],
+	},
+    {
+        "Name": "Unite License & Orange Unite Style: Dodrio",
+        "Data": {
+		"Release Date": "15-09-2022 00:00:00 UTC",
+        "AvailableUntil": "25-09-2022 23:59:59 UTC",
+        "Gems": "925",
+        "Discounted Gems Price": "786",
+        "Discount Start": "15-09-2022 00:00:00 UTC",
+        "Discount End": "25-09-2022 23:59:59 UTC",
+		},
+        "type": ["t_Tachie_Dodrio_00_187d0db485dabbfd", "t_Tachie_Dodrio_02_c22ebbc44c65e666"],
+	},
+    {
+        "Name": "Tuxedo Style: Gengar",
+        "Data": {
+		"Release Date": "26-09-2022 00:00:00 UTC",
+        "Gems": "400",
+		},
+        "type": ["t_Tachie_Gengar_06_845d0220745236ae"],
+	},
+    {
+        "Name": "Tuxedo Style: Mamoswine",
+        "Data": {
+		"Release Date": "03-10-2022 00:00:00 UTC",
+        "Gems": "1050",
+		},
+        "type": ["t_Tachie_Mamoswine_06_529a62eda476e871"],
+	},
+    {
+        "Name": "Wanderer Style: Slowbro",
+        "Data": {
+		"Release Date": "06-10-2022 00:00:00 UTC",
+        "Gems": "1050",
+		},
+        "type": ["t_Tachie_Slowbro_08_fe756184094f9310"],
+	},
+    {
+        "Name": "Guardian Style: Mr. Mime",
+        "Data": {
+		"Release Date": "08-09-2022 00:00:00 UTC",
+        "Gems": "2199",
+		},
+        "type": ["t_Tachie_MrMime_05_9f6c930e5bc3b683", "t_Avatar_frame_59"],
+	},
+    {
+        "Name": "Mew Set",
+        "Data": {
+		"Release Date": "06-09-2022 00:00:00 UTC",
+        "Gems": "699",
+        "Membership Gems Price": "629",
+		},
+        "type": ["t_Suit_female_461_2337652fbd569bc6", "t_Suit_male_461_ce26b5252931a9e2"],
+	},
+    {
+        "Name": "Pokémon Face Hat (Mew)",
+        "Data": {
+		"Release Date": "14-09-2022 00:00:00 UTC",
+        "Gems": "203",
+        "AeosTickets": "2030",
+        "Membership Aeos Tickets Price": "1827",
+        "Membership Gems Price": "183",
+		},
+        "type": ["t_Hat_467female__c988658d22950157"],
+	},
+    {
+        "Name": "Poet Beret",
+        "Data": {
+		"Release Date": "28-09-2022 00:00:00 UTC",
+        "Gems": "49",
+        "AeosTickets": "490",
+        "Membership Aeos Tickets Price": "441",
+        "Membership Gems Price": "44",
+		},
+        "type": ["t_Hat_female_485_bf976c91a4d1d239"],
+	},
+    {
+        "Name": "Backpack (Unite Ball)",
+        "Data": {
+		"Release Date": "22-09-2022 00:00:00 UTC",
+        "Gems": "98",
+        "AeosTickets": "980",
+        "Membership Aeos Tickets Price": "882",
+        "Membership Gems Price": "88",
+		},
+        "type": ["t_Bag_female_447_381208bb0cfd54e1"],
+	},
+    {
+        "Name": "Shoes (Unite Ball)",
+        "Data": {
+		"Release Date": "22-09-2022 00:00:00 UTC",
+        "Gems": "34",
+        "AeosTickets": "343",
+        "Membership Aeos Tickets Price": "309",
+        "Membership Gems Price": "31",
+		},
+        "type": ["t_Foot_female_448_bac0c25906dea661"],
+	},
+    {
+        "Name": "Guardian Top",
+        "Data": {
+		"Release Date": "10-10-2022 00:00:00 UTC",
+        "Gems": "123",
+        "AeosTickets": "1225",
+        "Membership Aeos Tickets Price": "1103",
+        "Membership Gems Price": "111",
+		},
+        "type": ["t_Top_female_465_4e699f7cbe286d3e", "t_Top_male_465_453de4f48da5b279"],
+	},
+    {
+        "Name": "Poet Top",
+        "Data": {
+		"Release Date": "05-10-2022 00:00:00 UTC",
+        "Gems": "123",
+        "AeosTickets": "1225",
+        "Membership Aeos Tickets Price": "1103",
+        "Membership Gems Price": "111",
+		},
+        "type": ["t_Top_female_464_df47c6a5327a8cfe", "t_Top_male_464_b0d1c1016d8bce37"],
+	},
+    {
+        "Name": "Sacred Style: Blastoise",
+        "Data": {
+		"Release Date": "22-09-2022 00:00:00 UTC",
+        "Gems": "2199",
+		},
+        "type": ["t_Tachie_Blastoise_09_d1dfc00ba1fbb725", "t_Avatar_frame_60"],
+	},
+    {
+        "Name": "Tuxedo Style: Talonflame",
+        "Data": {
+		"Release Date": "12-09-2022 00:00:00 UTC",
+        "Gems": "1200",
+		},
+        "type": ["t_Tachie_Talonflame_10_9d84dd45f19f909d"],
+	},
+    {
+        "Name": "Unite License: Scizor",
+        "Data": {
+        "Desc": "Grants permission to take Scizor into battle.",
+		"Release Date": "29-09-2022 00:00:00 UTC",
+        "AvailableUntil": "05-10-2022 23:59:59 UTC",
+        "Gems": "575",
+		},
+        "type": ["t_Tachie_Scizor_00_80326f344fecb374"],
+	},
+    {
+        "Name": "Unite License: Scizor",
+        "Data": {
+        "Desc": "Grants permission to take Scizor into battle.",
+		"Release Date": "06-10-2022 00:00:00 UTC",
+        "Coins": "14000",
+        "Gems": "575",
+    },        "type": ["t_Tachie_Scizor_00_80326f344fecb374"],}]
---
+++ Skins/data.js
@@ -0,0 +1,91 @@
+const badges=[    {
+        "Name": "Tuxedo Style: Garchomp",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Blue",
+        "IconPath": ["t_ShareGet_Garchomp_10_40cbe8a64b051a48"]
+    },
+    {
+        "Name": "Tuxedo Style: Glaceon",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Blue",
+        "IconPath": ["t_ShareGet_Glaceon_03_80cb5db3c1adb412"]
+    },
+    {
+        "Name": "Tuxedo Style: Gengar",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Gengar_06_f4d0f53246976801"]
+    },
+    {
+        "Name": "Tuxedo Style: Espeon",
+        "StageObtainWay": "Limited Time Activity",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Espeon_05_7c4877eabf45ec63"]
+    },
+    {
+        "Name": "Tuxedo Style: Mamoswine",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Blue",
+        "IconPath": ["t_ShareGet_Mamoswine_06_81d6d87d104d9a3b"]
+    },
+    {
+        "Name": "Guardian Style: Tsareena",
+        "StageObtainWay": "Subscription",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Tsareena_05_245fcf30e61d1c49"]
+    },
+    {
+        "Name": "Wanderer Style: Slowbro",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Blue",
+        "IconPath": ["t_ShareGet_Slowbro_08_9ccd32bd0a82f6b4"]
+    },
+    {
+        "Name": "Tuxedo Style: Venusaur",
+        "StageObtainWay": "Limited Time Activity",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Venusaur_07_a8512f47d67b95aa"]
+    },
+    {
+        "Name": "Purple Unite Style: Dodrio",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Dodrio_01_8d446929b2fed774"]
+    },
+    {
+        "Name": "Orange Unite Style: Dodrio",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Dodrio_02_3a5ffcce51785e39"]
+    },
+    {
+        "Name": "Elegant Style: Mew",
+        "StageObtainWay": "Battlepass",
+        "Quality": "t_quality_icon_Purple",
+	"IconPath": ["t_ShareGet_Mew_01_5102e93b4e8c0399", "t_Avatar_frame_58"]
+    },
+    {
+        "Name": "Sacred Style: Blastoise",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Purple",
+        "IconPath": ["t_ShareGet_Blastoise_09_fb9f99bc120c3d96", "t_Avatar_frame_60"]
+    },
+    {
+        "Name": "Guardian Style: Mr. Mime",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Purple",
+        "IconPath": ["t_ShareGet_MrMime_05_d39ee55c5ecfa49e", "t_Avatar_frame_59"]
+    },
+    {
+        "Name": "Wanderer Style: Azumarill",
+        "StageObtainWay": "Battlepass",
+        "Quality": "t_quality_icon_Green",
+        "IconPath": ["t_ShareGet_Azumarill_02_5d4a952afb3fcd48"]
+    },
+    {
+        "Name": "Tuxedo Style: Talonflame",
+        "StageObtainWay": "Gems",
+        "Quality": "t_quality_icon_Blue",
+        "IconPath": ["t_ShareGet_Talonflame_10_46e600c9161103c4"]
+    }
+]
